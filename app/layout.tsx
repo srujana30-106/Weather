2087bf83964704eb82a106b50b7c387dcd173a9f
sanakalakshmisrujana-cyber <sanakalakshmisrujana@gmail.com>
2026-01-06 15:52:49 +0530

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,4 @@
-
-
 import "./globals.css";
-
-export const metadata = {
-  title: "Weather App",
-  description: "Next.js Weather Forecast App",
-};
 
 export default function RootLayout({
   children,
@@ -14,9 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-gray-100 dark:bg-gray-900 text-black dark:text-white transition-colors">
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      "./app/**/*.{js,ts,jsx,tsx}", // For Next.js 13+ App Router
-      "./pages/**/*.{js,ts,jsx,tsx}", // For Page Router
+      "./app/**/*.{js,ts,jsx,tsx}",   // for Next.js app dir
+      "./pages/**/*.{js,ts,jsx,tsx}",
       "./components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
